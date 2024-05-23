4f48d5775fe289143338542eff2c357f63e54d3d
rjsah09 <rjsah09@naver.com>
2024-05-23 15:12:46 +0900

    CarVO 제작

--- src/com/parkosa/dao/AccountDAO.java
+++ src/com/parkosa/dao/AccountDAO.java
@@ -1,5 +1,65 @@
 package com.parkosa.dao;
 
-public class AccountDAO {
+import java.time.LocalDate;
 
+
+public class AccountDAO {
+	
+	private String phoneNumber;
+	private String password;
+	private String name;
+	private String eMail;
+	private LocalDate registerDate;
+	
+	//생성자
+	public AccountDAO(String phoneNumber, String password,
+			String name, String eMail, LocalDate registerDate) {
+		this.phoneNumber = phoneNumber;
+		this.password = password;
+		this.name = name;
+		this.eMail = eMail;
+		this.registerDate = registerDate;
+	}
+
+	//Getters and Setters
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String geteMail() {
+		return eMail;
+	}
+
+	public void seteMail(String eMail) {
+		this.eMail = eMail;
+	}
+
+	public LocalDate getRegisterDate() {
+		return registerDate;
+	}
+
+	public void setRegisterDate(LocalDate registerDate) {
+		this.registerDate = registerDate;
+	}
+	
 }
