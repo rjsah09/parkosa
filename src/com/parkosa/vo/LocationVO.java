e4d1bf6e3064e45fb500a1b0ad0163d5383eac7d
rjsah09 <rjsah09@naver.com>
2024-05-23 15:24:55 +0900

    LocationVO 제작

--- src/com/parkosa/vo/LocationVO.java
+++ src/com/parkosa/vo/LocationVO.java
@@ -1,5 +1,41 @@
 package com.parkosa.vo;
 
 public class LocationVO {
+	
+	private int id;
+	private String name;
+	private int upperId;
+	
+	public LocationVO(int id, String name, int upperId) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.upperId = upperId;
+	}
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getUpperId() {
+		return upperId;
+	}
+
+	public void setUpperId(int upperId) {
+		this.upperId = upperId;
+	}
+	
+	
 }
