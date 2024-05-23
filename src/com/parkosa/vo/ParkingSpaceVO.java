7d02c6c353db3b0f90dba486729cde2f41e8590a
Choi_YH96 <dbghks0425@gmail.com>
2024-05-23 16:01:02 +0900

    "ParkingSpaceVO 제작"

--- src/com/parkosa/vo/ParkingSpaceVO.java
+++ src/com/parkosa/vo/ParkingSpaceVO.java
@@ -1,5 +1,53 @@
 package com.parkosa.vo;
 
 public class ParkingSpaceVO {
+	
+	private int id;
+	private int parkinglotId;
+	private String decription;
+	private int feepolicyId;
+
+	
+	public ParkingSpaceVO(int id, 
+						int parkinglotId,
+						String decription,
+						int feepolicyId){
+		this.id = id;
+		this.parkinglotId = parkinglotId;
+		this.decription = decription;
+		this.feepolicyId = feepolicyId;
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getparkinglotId() {
+		return parkinglotId;
+	}
+
+	public void setparkinglotId(int parkinglotId) {
+		this.parkinglotId = parkinglotId;
+	}
+	
+	public int getdecription() {
+		return decription;
+	}
+
+	public void setdecription(String decription) {
+		this.decription = decription;
+	}
+	
+	public int getfeepolicyId() {
+		return feepolicyId;
+	}
+
+	public void setfeepolicyId(int feepolicyId) {
+		this.feepolicyId = feepolicyId;
+	}
 
 }
