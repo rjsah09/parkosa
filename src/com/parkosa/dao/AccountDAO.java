e8c28e1fbb2dc6d6e0b87bf10fd488e6a9fbd355
Choi_YH96 <dbghks0425@gmail.com>
2024-05-24 17:36:53 +0900

    "AccountDAO SignIn Function"

--- src/com/parkosa/dao/AccountDAO.java
+++ src/com/parkosa/dao/AccountDAO.java
@@ -6,6 +6,7 @@ import java.sql.Date;
 import java.sql.SQLException;
 
 import com.parkosa.connection.DBConnection;
+import com.parkosa.dto.SignInDTO;
 import com.parkosa.vo.AccountVO;
 
 
@@ -64,6 +65,35 @@ public class AccountDAO {
 		}
 		
 		return true;
+	}
+	
+	public String signIn(SignInDTO signInDTO) {
+		
+		String function = "{ ? = call account_pack.FUNCTION FN_NUMBER_PW_CHECK(?,?) }";
+		
+		try {
+			Connection conn = DBConnection.getConnection();
+			CallableStatement callableStatement = conn.prepareCall(function);
+			
+			//변수 할당
+			callableStatement.registerOutParameter(1, java.sql.Types.NUMERIC);
+			callableStatement.setString(2, signInDTO.getPhoneNumber());
+			callableStatement.setString(3, signInDTO.getPassword());
+			callableStatement.executeUpdate();
+			
+			String phoneNumber = callableStatement.getString(1);
+			
+			return phoneNumber;
+			
+		} catch (SQLException e) {
+			System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+		}
+		
+		return null;
 	}
 	
 }
