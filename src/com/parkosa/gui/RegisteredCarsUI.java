3d8c05466fd22ce3dba176ea55b2620a849689ff
Choi_YH96 <dbghks0425@gmail.com>
2024-05-27 21:43:40 +0900

    "삭제버튼 추가"

--- src/com/parkosa/gui/RegisteredCarsUI.java
+++ src/com/parkosa/gui/RegisteredCarsUI.java
@@ -1,7 +1,10 @@
 package com.parkosa.gui;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.List;
 
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
@@ -9,6 +12,7 @@ import javax.swing.JTextField;
 import com.parkosa.dao.AccountDAO;
 import com.parkosa.dao.CarDAO;
 import com.parkosa.dto.RegisteredCarDTO;
+import com.parkosa.sign.SignedAccount;
 
 public class RegisteredCarsUI extends UI {
 	
@@ -24,15 +28,30 @@ public class RegisteredCarsUI extends UI {
         nameLabel.setBounds(100, 10, 200, 50);
         add(nameLabel);
         
+        JButton cancelButton = new JButton("뒤로가기");
+        cancelButton.setBounds(40, 40, 295, 25);
+        add(cancelButton);
+        
+        cancelButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            	SignedAccount.signOut();
+            	GUIController.changeUI(ui, new ParkKosaUI());
+            }
+        });
+        
     	CarDAO carDAO = new CarDAO();
     	List <RegisteredCarDTO> registeredCars = carDAO.getRegisteredCars();
     	String[] items = new String[carDAO.getRegisteredCars().size()];
+    	
     	for (int i = 0 ; i < registeredCars.size() ; i++) {
     		JTextField carList = new JTextField();
     		carList.setText(registeredCars.get(i).getCarCode()+" "+
     						registeredCars.get(i).getCarTypeName());
-    		carList.setBounds(60, 100+(i*30), 100, 25);
+    		carList.setBounds(60, 100+(i*40), 240, 30);
     		add(carList);
+    		JButton deleteButton = new JButton("삭제");
+    		carList.setBounds(330, 100+(i*40), 40, 30);
+    		add(deleteButton);
     	}
     	
         
