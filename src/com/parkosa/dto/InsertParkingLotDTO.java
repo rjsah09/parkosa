47a5327ccbfbe806af4e1ffa02959bdc222784f4
rjsah09 <rjsah09@naver.com>
2024-05-28 10:40:47 +0900

    InsertParkingLotDTO 수정

--- src/com/parkosa/dto/InsertParkingLotDTO.java
+++ src/com/parkosa/dto/InsertParkingLotDTO.java
@@ -5,12 +5,14 @@ public class InsertParkingLotDTO {
 	private String telNumber;
 	private int locationId;
 	private String address;
+	private String imageLink;
 	
-	public InsertParkingLotDTO(String name, String telNumber, int locationId, String address) {
+	public InsertParkingLotDTO(String name, String telNumber, int locationId, String address, String imageLink) {
 		this.telNumber = telNumber;
 		this.name = name;
 		this.locationId = locationId;
 		this.address = address;
+		this.imageLink = imageLink;
 	}
 	
 	public String getTelNumber() {
@@ -27,6 +29,10 @@ public class InsertParkingLotDTO {
 	
 	public String getAddress() {
 		return address;
+	}
+	
+	public String getImageLink() {
+		return imageLink;
 	}
 	
 }
