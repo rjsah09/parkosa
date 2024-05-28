aa310fe4944e37a5617026c6b866112da8631923
rjsah09 <rjsah09@naver.com>
2024-05-28 14:55:52 +0900

    ImageSaver 클래스 생성

---
+++ src/com/parkosa/image/ImageSaver.java
@@ -0,0 +1,10 @@
+package com.parkosa.image;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class ImageSaver {
+
+}
