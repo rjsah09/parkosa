99fa4fd7a5c71bd126c509550ca5e2bd9dd0d1bc
Choi_YH96 <dbghks0425@gmail.com>
2024-05-29 13:52:20 +0900

    ParkingSpaceDAO수정

--- src/com/parkosa/dao/ParkingSpaceDAO.java
+++ src/com/parkosa/dao/ParkingSpaceDAO.java
@@ -38,62 +38,29 @@ public class ParkingSpaceDAO {
 
     }
     
-    public List<RegisteredParkingSpaceDTO> getRegisteredParkingSpaces() {
-		List <RegisteredParkingSpaceDTO> registeredParkingSpaces = new ArrayList<>();
-        String sql = "{ call parking_space_pack.get_parking_space_info(?) }";
-        
-        try {
-            Connection conn = DBConnection.getConnection();
-            CallableStatement callableStatement = conn.prepareCall(sql);
-            //변수 할당
-            callableStatement.registerOutParameter(1, OracleTypes.CURSOR);
-            callableStatement.execute();
-
-            ResultSet rs = (ResultSet) callableStatement.getObject(5);
-            
-            while(rs.next()){
-            	String carTypeName = rs.getString("car_type_name");
-                int incereaseMinute = rs.getInt("fee_policy_increase_minute");
-                int increaseFee = rs.getInt("fee_policy_increase_fee");
-                int maximumTime = rs.getInt("fee_policy_maximum_time");
-                String description = rs.getString("parking_space_description");
-                
-                
-                registeredParkingSpaces.add(new RegisteredParkingSpaceDTO(carTypeName, incereaseMinute, increaseFee, maximumTime, description ));
-            }
-        } catch (SQLException e) {
-            System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
-            e.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-        }
-
-        return registeredParkingSpaces;
-	}
-    
-    public ArrayList<RegisteredParkingSpaceDTO> listParkingSpace() {
+    public ArrayList<RegisteredParkingSpaceDTO> listParkingSpace(int parkingLotId) {
         ArrayList<RegisteredParkingSpaceDTO> listParkingSpace = new ArrayList<>();
-        String sql = "{call parking_space_pack.get_parking_space_info(?)}";
+        String sql = "{call parking_space_pack.get_parking_space_info(?, ?)}";
 
         try {
             Connection conn = DBConnection.getConnection();
             CallableStatement callableStatement = conn.prepareCall(sql);
             //변수 할당
-            callableStatement.registerOutParameter(1, OracleTypes.CURSOR);
+            callableStatement.setInt(1, parkingLotId);
+            callableStatement.registerOutParameter(2, OracleTypes.CURSOR);
             callableStatement.execute();
 
-            ResultSet rs = (ResultSet) callableStatement.getObject(1);
+            ResultSet rs = (ResultSet) callableStatement.getObject(2);
 
             while (rs.next()) {
-                String name = rs.getString("name");
+                String name = rs.getString("car_type_name");
                 int increaseMinute = rs.getInt("increase_minute");
                 int increaseFee = rs.getInt("increase_fee");
                 int maximumTime = rs.getInt("maximum_time");
                 String decription = rs.getString("decription");
 				listParkingSpace.add(new RegisteredParkingSpaceDTO(name, increaseMinute, increaseFee, maximumTime, decription));
             }
-
+            	
 
         } catch (SQLException e) {
             System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
