d45ddbb4e36c36efb19538de0110437392e618f9
gaeng <momo5736@naver.com>
2024-05-29 11:11:52 +0900

    ReservationDTO / DAO insertData

--- src/com/parkosa/dao/ReservationDAO.java
+++ src/com/parkosa/dao/ReservationDAO.java
@@ -1,5 +1,33 @@
 package com.parkosa.dao;
 
+import com.parkosa.connection.DBConnection;
+import com.parkosa.dto.InsertReservationDTO;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+
 public class ReservationDAO {
 
+    public void insertReservation(InsertReservationDTO insertReservationDTO) {
+        String proc = "{ call RESERVATION_PACK.insert_reservation (?,?,?,?) }";
+
+        try {
+            Connection conn = DBConnection.getConnection();
+            CallableStatement callableStatement = conn.prepareCall(proc);
+
+            callableStatement.setDate(1, insertReservationDTO.getStartTime());
+            callableStatement.setDate(2, insertReservationDTO.getEndTime());
+            callableStatement.setString(3, insertReservationDTO.getCarCode());
+            callableStatement.setInt(4, insertReservationDTO.getParkingLotId());
+
+            callableStatement.executeUpdate();
+        } catch (SQLException e) {
+            System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/com/parkosa/dto/InsertReservationDTO.java
+++ src/com/parkosa/dto/InsertReservationDTO.java
@@ -1,48 +1,70 @@
 package com.parkosa.dto;
 
-import java.time.LocalDateTime;
+import oracle.sql.DATE;
+
+import java.sql.Date;
 
 public class InsertReservationDTO {
 	
-	private LocalDateTime startTime;
-	private LocalDateTime endTime;
+	private Date startTime;
+	private Date endTime;
 	private int totalAmount;
 	private String carCode;
 	private int parkingSpaceId;
 	private int parkingLotId;
-	
-	public InsertReservationDTO(LocalDateTime startTime, LocalDateTime endTime, int totalAmount, String carCode,
-			int parkingSpaceId, int parkingLotId) {
+
+	public InsertReservationDTO(Date startTime, Date endTime, String carCode, int parkingLotId) {
 		this.startTime = startTime;
 		this.endTime = endTime;
-		this.totalAmount = totalAmount;
 		this.carCode = carCode;
-		this.parkingSpaceId = parkingSpaceId;
 		this.parkingLotId = parkingLotId;
 	}
 
-	public LocalDateTime getStartTime() {
+	public Date getStartTime() {
 		return startTime;
 	}
 
-	public LocalDateTime getEndTime() {
+	public void setStartTime(Date startTime) {
+		this.startTime = startTime;
+	}
+
+	public Date getEndTime() {
 		return endTime;
 	}
-	
+
+	public void setEndTime(Date endTime) {
+		this.endTime = endTime;
+	}
+
 	public int getTotalAmount() {
 		return totalAmount;
+	}
+
+	public void setTotalAmount(int totalAmount) {
+		this.totalAmount = totalAmount;
 	}
 
 	public String getCarCode() {
 		return carCode;
 	}
 
+	public void setCarCode(String carCode) {
+		this.carCode = carCode;
+	}
+
 	public int getParkingSpaceId() {
 		return parkingSpaceId;
+	}
+
+	public void setParkingSpaceId(int parkingSpaceId) {
+		this.parkingSpaceId = parkingSpaceId;
 	}
 
 	public int getParkingLotId() {
 		return parkingLotId;
 	}
-	
+
+	public void setParkingLotId(int parkingLotId) {
+		this.parkingLotId = parkingLotId;
+	}
 }
